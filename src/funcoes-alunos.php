ea948f1bcda47203c0c5410f9000839c4178b496
vitor.gama <vitorggam@hotmail.com>
2023-09-01 08:26:23 -0300

    funcoes-alunos

--- src/funcoes-alunos.php
+++ src/funcoes-alunos.php
@@ -1,25 +1,29 @@
-<?php
-require_once "conecta_php";
+<?php 
+require_once "conecta.php";
 
-//function lerAlunos( PDO $conexao ){
-//$sql = "SELECT * FROM alunos ORDER BY nome";
+function lerAlunos(PDO $conexao):array{
+    $sql = "SELECT * FROM alunos ORDER BY nome";
 
-//try {
-  //  $consulta = $conexao->prepare($sql);
-    //$consulta->execute();
-    //$resultado = $consulta->fetchAll(PDO::FETCH_ASSOC)
-//} catch (Exception $erro) {
-  //  die
-//}
+    try {
+        $consulta = $conexao->prepare($sql);
+        $consulta->execute();
+        $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC); 
+    } catch (Exception $erro) {
+        die("Erro ao carregar alunos: ".$erro->getMessage());
+    }
+    return $resultado;
+}
 
-//}
+function inserirAluno(PDO $conexao, string $nome, float $nota1, float $nota2):void {
+    $sql = "INSERT INTO alunos(nome, nota1, nota2) VALUE (:nome, :nota1, :nota2)";
 
-function inserirAluno( PDO $conexao, string $nome, int $nota1, int $nota2){
-  $sql = "INSERT TO alunos(nome, nota1, nota2) VALUES(:nome, :nota1, :nota2)";
-  
-
-
-
-
-
-?>
+    try {
+        $consulta = $conexao->prepare($sql);
+        $consulta->bindValue(":nome", $nome, PDO::PARAM_STR);
+        $consulta->bindValue("nota1", $nota1, PDO::PARAM_STR);
+        $consulta->bindValue("nota2", $nota2, PDO::PARAM_STR);
+        $consulta->execute();
+    } catch (Exception $erro) {
+        die("Erro ao inserir: ".$erro->getMessage());
+    }
+}
