4cabc00b28782641ffd55b1dac4ad0f977f337f7
vitor.gama <vitorggam@hotmail.com>
2023-09-06 09:07:33 -0300

    update atualiza-campo.js

--- src/atualiza-campo.js
+++ src/atualiza-campo.js
@@ -9,18 +9,28 @@ let primeira, segunda, media, situacao;
 function atualizarMedia(nota1, nota2){
     return (nota1 + nota2) / 2;
 }
+
 function atualizarSituacao(resultadoDaMedia){
-    if(resultadoDaMedia >= 7 ){
+    if(resultadoDaMedia >= 7){
         situacao = "aprovado";
-    }else if(resultadoDaMedia >= 5){
+    } else if(resultadoDaMedia >= 5){
         situacao = "recuperacao";
-    }else{
+    } else {
         situacao = "reprovado";
     }
     return situacao;
 }
 
 inputPrimeira.addEventListener("input", function(){
-    console.log(this.value);
+    primeira = parseFloat(this.value);
+    media = atualizarMedia(primeira, parseFloat(inputSegunda.value));
+    inputMedia.value = media.toFixed(2);
+    inputSituacao.value = atualizarSituacao(media);
 });
 
+inputSegunda.addEventListener("input", function(){
+    segunda = parseFloat(this.value);
+    media = atualizarMedia(parseFloat(inputPrimeira.value), segunda);
+    inputMedia.value = media.toFixed(2);
+    inputSituacao.value = atualizarSituacao(media);
+});
